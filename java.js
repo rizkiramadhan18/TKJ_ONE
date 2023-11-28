475480d5ec7558e7d3a708806381e823f6824394
RR018 <146320641+rizkiramadhan18@users.noreply.github.com>
2023-11-28 22:10:58 +0700

    Add files via upload

--- java.js
+++ java.js
@@ -1,4 +1,9 @@
-let slideIndex = 0;
+  function scrollToTop() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+    
+ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
@@ -18,3 +23,16 @@ function showSlides() {
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+      const scrollBtn = document.getElementById('scrollBtn');
+
+      window.onscroll = function () {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          scrollBtn.style.display = 'block';
+        } else {
+          scrollBtn.style.display = 'none';
+        }
+      };
+    });
+
+    
